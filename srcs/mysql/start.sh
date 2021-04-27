b386c8a8cf67cd369911bebcaf8405699aeeabfb
Eugene Vlasov <vlasov3454@yandex.ru>
2021-04-28 01:59:39 +0300

    4

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
-# /usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# mysql_install_db --user=root --datadir=/var/lib/mysql
+/etc/init.d/mysql setup
+/etc/init.d/mysql start
+
 # mysql -u root
-# echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' WITH GRANT OPTION;"| mysql -u root --skip-password
-# echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-
-/usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql
-# /etc/init.d/mariadb setup
-# rc-service mariadb start
-
 echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' WITH GRANT OPTION;"| mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-# echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
-mysql wordpress -u root
-
 
 tail -f /dev/null
