9510cfeb9ba6ca32467da88544616ae2a539a311
Eugene Vlasov <vlasov3454@yandex.ru>
2021-04-30 00:53:16 +0300

    nginx+wp+msql+php

--- setup.sh
+++ setup.sh
@@ -12,11 +12,10 @@ eval $(minikube docker-env)
 docker build -t nginx-image ./srcs/nginx
 docker build -t mysql-image ./srcs/mysql
 docker build -t wordpress-image ./srcs/wordpress
-
-
-
+docker build -t phpmyadmin-image ./srcs/phpmyadmin
 
 kubectl apply -f srcs/yaml/nginx.yaml
 kubectl apply -f srcs/yaml/mysql.yaml
 kubectl apply -f srcs/yaml/wordpress.yaml
+kubectl apply -f srcs/yaml/phpmyadmin.yaml
 # ./srcs/nginx/start.sh
