b4ba5b9e951068ab7cb67939f2ccb77e5465a8c0
Eugene Vlasov <vlasov3454@yandex.ru>
2021-05-03 00:44:48 +0300

    5

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# minikube setup, enable and metallb
+# minikube setup, enable LB
 minikube stop
 minikube delete
 minikube start --vm-driver=virtualbox
@@ -14,8 +14,8 @@ docker build -t mysql-image ./srcs/mysql
 docker build -t wordpress-image ./srcs/wordpress
 docker build -t phpmyadmin-image ./srcs/phpmyadmin
 
+# apply configs
 kubectl apply -f srcs/yaml/nginx.yaml
 kubectl apply -f srcs/yaml/mysql.yaml
 kubectl apply -f srcs/yaml/wordpress.yaml
 kubectl apply -f srcs/yaml/phpmyadmin.yaml
-# ./srcs/nginx/start.sh
--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,4 +1,5 @@
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql
-/usr/bin/mysqld --user=root --init_file=/init_db
-
+/usr/bin/mysqld --user=root --init_file=/init_db &
+sleep 10
+/usr/bin/mysql wordpress -u root < /wordpress.sql
 tail -f /dev/null
